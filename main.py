63b1e018a3ab0d3f82953d9f60e26aec1953235f
OPITMaria <maria.b@students.opit.com>
2024-12-11 15:37:52 -0800

    added the analysis for the internet only dataset, and run the models. also, did a correlation matrix for the all internet customers. I looked for outliers as part of preprocessing (but in my own notebook)

--- main.py
+++ main.py
@@ -10,15 +10,19 @@ df = data_loader.check_data_quality()
 
 # Preprocess data
 preprocessor = DataPreprocessor(df)
-df_internet, df_phone = preprocessor.split_by_service()
+df_internet, df_phone, df_internet_only = preprocessor.split_by_service()
 
 # Preprocess categorical columns
 df_internet = preprocessor.preprocess_categorical(df_internet)
 df_phone = preprocessor.preprocess_categorical(df_phone)
+df_internet_only = preprocessor.preprocess_categorical(df_internet_only)
+
 
 # Format column names
 df_internet.columns = df_internet.columns.str.lower().str.replace(' ', '_')
 df_phone.columns = df_phone.columns.str.lower().str.replace(' ', '_')
+df_internet_only.columns = df_internet_only.columns.str.lower().str.replace(' ', '_')
+
 
 # Split and process data
 splitter = DataSplitter(df_internet, df_phone, 0.2)
@@ -33,7 +37,25 @@ df_test_internet = splitter.revert_to_booleans(df_test_internet)
 df_train_phone = splitter.revert_to_booleans(df_train_phone)
 df_test_phone = splitter.revert_to_booleans(df_test_phone)
 
+
 df_train_internet.to_csv('preprocessed_data/df_train_internet.csv', index = False)
 df_test_internet.to_csv('preprocessed_data/df_test_internet.csv', index = False)
 df_train_phone.to_csv('preprocessed_data/df_train_phone.csv', index = False)
 df_test_phone.to_csv('preprocessed_data/df_test_phone.csv', index = False)
+
+# Train-test split for internet-only customers
+splitter = DataSplitter(df_internet_only, None, test_size=0.2)
+df_train_internet_only, df_test_internet_only, _, _ = splitter.train_test_split()
+
+# Impute and revert to booleans for internet-only dataset
+df_train_internet_only, df_test_internet_only = splitter.impute_missing_values(df_train_internet_only, df_test_internet_only)
+
+df_train_internet_only = splitter.revert_to_booleans(df_train_internet_only)
+df_test_internet_only = splitter.revert_to_booleans(df_test_internet_only)
+
+# Save internet-only datasets
+df_train_internet_only.to_csv('preprocessed_data/df_train_internet_only.csv', index=False)
+df_test_internet_only.to_csv('preprocessed_data/df_test_internet_only.csv', index=False)
+
+
+print("Internet-only training dataset saved as 'preprocessed_data/df_train_internet_only.csv'")
--- preprocess.py
+++ preprocess.py
@@ -43,7 +43,7 @@ class DataLoader:
 class DataPreprocessor:
     def __init__(self, df):
         self.df = df
-        self.internet_service_cols = ['OnlineSecurity', 'OnlineBackup', 'DeviceProtection', 'TechSupport', 'StreamingTV', 'StreamingMovies']
+        self.internet_service_cols = ['InternetService', 'OnlineSecurity', 'OnlineBackup', 'DeviceProtection', 'TechSupport', 'StreamingTV', 'StreamingMovies']
         self.phone_service_cols = ['MultipleLines']
     
     def split_by_service(self):
@@ -53,8 +53,12 @@ class DataPreprocessor:
         
         df_phone = self.df[self.df['PhoneService'] != 'No'].copy()
         df_phone = df_phone.drop(columns=self.internet_service_cols + ['PhoneService'])
+
+        # Internet-only customers (InternetService != 'No' AND PhoneService == 'No')
+        df_internet_only = self.df[(self.df['InternetService'] != 'No') & (self.df['PhoneService'] == 'No')].copy()
         
-        return df_internet, df_phone
+        return df_internet, df_phone, df_internet_only
+
     
     @staticmethod
     def preprocess_categorical(df):
@@ -84,63 +88,212 @@ class DataPreprocessor:
         
         return processed_df
 
+# class DataSplitter:
+#     def __init__(self, df_internet, df_phone, test_size: float):
+#         self.df_internet = df_internet
+#         self.df_phone = df_phone
+#         self.num_col = ['tenure', 'totalcharges', 'monthlycharges']
+#         self.target = 'churn'
+#         self.test_size = test_size
+    
+#     def train_test_split(self):
+#         common_index = self.df_phone.index.intersection(self.df_internet.index)
+#         df_phone_internet_common_train = self.df_phone.loc[common_index]
+#         y = df_phone_internet_common_train[self.target]
+#         train_index, test_index = train_test_split(common_index, test_size=0.2, stratify=y, random_state=42)
+
+#         df_phone_common_train = self.df_phone.loc[train_index]
+#         df_internet_common_train = self.df_internet.loc[train_index]
+#         df_phone_common_test = self.df_phone.loc[test_index]
+#         df_internet_common_test = self.df_internet.loc[test_index]
+
+#         df_phone_only = self.df_phone.drop(index = common_index)
+#         df_internet_only = self.df_internet.drop(index = common_index)
+
+#         df_phone_only_train, df_phone_only_test = train_test_split(
+#             df_phone_only, 
+#             test_size=0.2, 
+#             stratify=df_phone_only[self.target],
+#             random_state=42
+#         )
+
+#         df_internet_only_train, df_internet_only_test = train_test_split(
+#             df_internet_only, 
+#             test_size=0.2, 
+#             stratify=df_internet_only[self.target],
+#             random_state=42
+#         )
+
+#         df_train_internet = pd.concat([df_internet_common_train, df_internet_only_train])
+#         df_test_internet = pd.concat([df_internet_common_test, df_internet_only_test])
+#         df_train_phone = pd.concat([df_phone_common_train, df_phone_only_train])
+#         df_test_phone = pd.concat([df_phone_common_test, df_phone_only_test])
+
+              
+#         return df_train_internet, df_test_internet, df_train_phone, df_test_phone
+
+# DATASPLITTER, MARIA'S EDITS
 class DataSplitter:
-    def __init__(self, df_internet, df_phone, test_size: float):
+    def __init__(self, df_internet=None, df_phone=None, test_size: float = 0.2):
         self.df_internet = df_internet
         self.df_phone = df_phone
         self.num_col = ['tenure', 'totalcharges', 'monthlycharges']
         self.target = 'churn'
         self.test_size = test_size
-    
+
+    def split_dataset(self, df):
+        """Splits a given dataset into training and testing sets."""
+        return train_test_split(
+            df,
+            test_size=self.test_size,
+            stratify=df[self.target] if self.target in df else None,
+            random_state=42
+        )
+
     def train_test_split(self):
-        common_index = self.df_phone.index.intersection(self.df_internet.index)
-        df_phone_internet_common_train = self.df_phone.loc[common_index]
-        y = df_phone_internet_common_train[self.target]
-        train_index, test_index = train_test_split(common_index, test_size=0.2, stratify=y, random_state=42)
+        """Splits internet and phone datasets into train/test sets."""
+        df_train_internet, df_test_internet = None, None
+        df_train_phone, df_test_phone = None, None
 
-        df_phone_common_train = self.df_phone.loc[train_index]
-        df_internet_common_train = self.df_internet.loc[train_index]
-        df_phone_common_test = self.df_phone.loc[test_index]
-        df_internet_common_test = self.df_internet.loc[test_index]
+        # Process internet dataset if provided
+        if self.df_internet is not None:
+            if self.df_phone is not None:
+                # Handle common indices
+                common_index = self.df_phone.index.intersection(self.df_internet.index)
+                df_internet_common = self.df_internet.loc[common_index]
+                df_internet_only = self.df_internet.drop(index=common_index)
+                train_index, test_index = train_test_split(
+                    common_index,
+                    test_size=self.test_size,
+                    stratify=self.df_phone.loc[common_index, self.target] if self.target in self.df_phone else None,
+                    random_state=42
+                )
+                # Split the common subset
+                df_train_internet_common = df_internet_common.loc[train_index]
+                df_test_internet_common = df_internet_common.loc[test_index]
+                # Split internet-only subset
+                df_train_internet_only, df_test_internet_only = self.split_dataset(df_internet_only)
+                # Combine results
+                df_train_internet = pd.concat([df_train_internet_common, df_train_internet_only])
+                df_test_internet = pd.concat([df_test_internet_common, df_test_internet_only])
+            else:
+                # Split the entire internet dataset
+                df_train_internet, df_test_internet = self.split_dataset(self.df_internet)
 
-        df_phone_only = self.df_phone.drop(index = common_index)
-        df_internet_only = self.df_internet.drop(index = common_index)
+        # Process phone dataset if provided
+        if self.df_phone is not None:
+            if self.df_internet is not None:
+                # Handle common indices
+                df_phone_common = self.df_phone.loc[common_index]
+                df_phone_only = self.df_phone.drop(index=common_index)
+                # Split the common subset
+                df_train_phone_common = df_phone_common.loc[train_index]
+                df_test_phone_common = df_phone_common.loc[test_index]
+                # Split phone-only subset
+                df_train_phone_only, df_test_phone_only = self.split_dataset(df_phone_only)
+                # Combine results
+                df_train_phone = pd.concat([df_train_phone_common, df_train_phone_only])
+                df_test_phone = pd.concat([df_test_phone_common, df_test_phone_only])
+            else:
+                # Split the entire phone dataset
+                df_train_phone, df_test_phone = self.split_dataset(self.df_phone)
 
-        df_phone_only_train, df_phone_only_test = train_test_split(
-            df_phone_only, 
-            test_size=0.2, 
-            stratify=df_phone_only[self.target],
-            random_state=42
-        )
-
-        df_internet_only_train, df_internet_only_test = train_test_split(
-            df_internet_only, 
-            test_size=0.2, 
-            stratify=df_internet_only[self.target],
-            random_state=42
-        )
-
-        df_train_internet = pd.concat([df_internet_common_train, df_internet_only_train])
-        df_test_internet = pd.concat([df_internet_common_test, df_internet_only_test])
-        df_train_phone = pd.concat([df_phone_common_train, df_phone_only_train])
-        df_test_phone = pd.concat([df_phone_common_test, df_phone_only_test])
-        
         return df_train_internet, df_test_internet, df_train_phone, df_test_phone
     
     def impute_missing_values(self, df_train, df_test):
-        """Impute missing values using KNN"""
+        """Impute missing values using KNN for numeric columns only."""
+        # Select only numeric columns for imputation
+        numeric_cols = df_train.select_dtypes(include=['float64', 'int64']).columns
+
+        # Initialize KNNImputer
         imputer = KNNImputer(n_neighbors=10)
-        df_train[:] = imputer.fit_transform(df_train)
-        df_test[:] = imputer.transform(df_test)
-        
+
+        # Impute numeric columns in train and test datasets
+        df_train_imputed = pd.DataFrame(
+            imputer.fit_transform(df_train[numeric_cols]),
+            columns=numeric_cols,
+            index=df_train.index
+        )
+        df_test_imputed = pd.DataFrame(
+            imputer.transform(df_test[numeric_cols]),
+            columns=numeric_cols,
+            index=df_test.index
+        )
+
+        # Update original DataFrames with imputed values
+        df_train.update(df_train_imputed)
+        df_test.update(df_test_imputed)
+
         return df_train, df_test
+    
+    def revert_to_booleans(self, df):
+        """Converts specific numeric columns back to boolean."""
+        bool_features = [col for col in df.columns if col not in self.num_col]
+        for col in bool_features:
+            if col in df.columns:
+                # Convert non-zero and non-null values to True, others to False
+                df[col] = df[col].notnull() & (df[col] != 0)
+        return df
+ 
+
+ # Maria's edits END
+
+
+    # def impute_missing_values(self, df_train, df_test):
+    #     """Impute missing values using KNN and cast to appropriate types."""
+    #     imputer = KNNImputer(n_neighbors=10)
+        
+    #     # Impute missing values
+    #     df_train_imputed = pd.DataFrame(imputer.fit_transform(df_train), columns=df_train.columns)
+    #     df_test_imputed = pd.DataFrame(imputer.transform(df_test), columns=df_test.columns)
+        
+    #     # Restore the original dtypes
+    #     for col in df_train.columns:
+    #         original_dtype = df_train[col].dtype
+    #         if original_dtype == 'boolean':  # Explicitly cast boolean columns
+    #             df_train_imputed[col] = df_train_imputed[col].astype(bool)
+    #             df_test_imputed[col] = df_test_imputed[col].astype(bool)
+    #         else:  # Restore other types as necessary
+    #             df_train_imputed[col] = df_train_imputed[col].astype(original_dtype)
+    #             df_test_imputed[col] = df_test_imputed[col].astype(original_dtype)
+
+    #     return df_train_imputed, df_test_imputed
+
+# Maria's Edits
+def impute_missing_values(self, df_train, df_test):
+    """Impute missing values using KNN for numeric columns only."""
+    numeric_cols = df_train.select_dtypes(include=['float64', 'int64']).columns
+
+    # Apply imputer only to numeric columns
+    imputer = KNNImputer(n_neighbors=10)
+    df_train_numeric = pd.DataFrame(
+        imputer.fit_transform(df_train[numeric_cols]),
+        columns=numeric_cols,
+        index=df_train.index
+    )
+    df_test_numeric = pd.DataFrame(
+        imputer.transform(df_test[numeric_cols]),
+        columns=numeric_cols,
+        index=df_test.index
+    )
+
+    # Replace numeric columns in original DataFrames
+    df_train.update(df_train_numeric)
+    df_test.update(df_test_numeric)
+
+    return df_train, df_test
+# Maria's edits END
+
+    
     
     def revert_to_booleans(self, df):
         """Convert numerical columns back to boolean"""
         bool_features = [col for col in df.columns if col not in self.num_col]
         for col in bool_features:
             if col in df.columns:
-                df[col] = df[col].astype('boolean')
+                df[col] = df[col].astype(bool)
         return df
+
+
 
 
